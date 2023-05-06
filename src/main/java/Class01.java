18650b60ecea5499b8b9e1c4b6828ca3a8b703f9
mevluto914 <mevluto914@gmail.com>
2023-05-06 20:40:46 +0200

    Version1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,20 @@
+public class Class01 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Version 1 için işlemler tamam!");
+        //Version 2 çalışmaları...
+        int z = 4;
+        System.out.println(x+y+z);
+        //V3
+        System.out.println("V3 kodları...");
+        //V4
+        System.out.println("V4 çalışmaları");
+        //V5 kodlarım...
+
+
+
+    }
+}
