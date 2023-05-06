936919bd0364ab197a38c900691fb06c6b1f1cc3
mevluto914 <mevluto914@gmail.com>
2023-05-06 20:45:16 +0200

    Version3

--- src/main/java/Class2.java
+++ src/main/java/Class2.java
@@ -2,6 +2,7 @@ public class Class2 {
     public static void main(String[] args) {
         //V1 calismalarim...
         //V2 calismalarim... cok calistim ama
+        System.out.println("V3 kodlarim...");// V3 kodlari
 
     }
 }
